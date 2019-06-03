901df86a538721999b0fd6a9d477db5b70534908
FarhadMohamad <art_ismat@hotmail.com>
2019-06-03 11:59:36 +0200

    z

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
-const assert = require('assert');
-const should = require('should');
+const assert = require('assert');zxczxc
+const should = require('should');zxczxcx
 const expect = require('chai').expect;
 const { pizzas } = require('../data/pizzas')
 
-
+zxc
 console.log(Object.keys(pizzas[0]).length);
 
     describe('Scenario 1', function() {
@@ -11,12 +11,12 @@ console.log(Object.keys(pizzas[0]).length);
         it('Pizza 2 is pepperoni', function() {
            expect(pizzaName).to.equal('pepperoni');
 
-        });
+        });zxc
 
         it('Length Equals', function() {
             expect(Object.keys(pizzas[0])).to.have.lengthOf(3);
 
-        });
+        });zxc
 
         it('Property Equals', function() {
             expect(pizzas[0][0]).to.have.property('name').with.lengthOf(7);
